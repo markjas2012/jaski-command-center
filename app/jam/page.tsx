34fa260337b2bf019550f3069684061a3c028204
markjas2012 <markjas@gmail.com>
2026-08-04 14:12:10 -0500

    Sprint 17.17g lock Jam Room festival design

--- app/jam/page.tsx
+++ app/jam/page.tsx
@@ -1,11 +1,12 @@
 import Sidebar from "../../components/Sidebar";
 import JamHero from "../../components/JamHero";
+import theme from "./jam-festival.module.css";
 
 export default function JamPage() {
   return (
-    <div className="app-shell">
+    <div className={`app-shell ${theme.festivalShell}`}>
       <Sidebar activePage="jam" />
-      <section className="content-shell">
+      <section className={`content-shell ${theme.festival}`}>
         <JamHero />
       </section>
     </div>
