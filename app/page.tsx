dae866d8346907f4fd6d6db08e6b99bb530c4565
markjas2012 <markjas@gmail.com>
2026-08-05 12:30:35 -0500

    Sprint 17.19i lock luxury lodge homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Sidebar from "../components/Sidebar";
 import LiveDashboard from "../components/LiveDashboard";
+import "./home-lodge.css";
+import "./home-lodge-final.css";
 
 export default function Home() {
   return (
-    <main className="app-shell">
+    <main className="app-shell home-lodge-shell">
       <Sidebar activePage="home" />
 
       <section className="content-stage" aria-label="Command Center content">
